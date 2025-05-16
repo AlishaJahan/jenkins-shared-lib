77174ffd90ad94c43081b87193f09b68fb09d43b
Alisha Jahan <117798701+AlishaJahan@users.noreply.github.com>
2025-05-16 15:31:20 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,3 +1,16 @@
-def buildImage(String imageName, String tag = 'latest') {
-    sh "docker build -t ${imageName}:${tag} ."
+def call(Map config = [:]) {
+    def imageName   = config.get('imageName') ?: error("Image name is required")
+    def imageTag    = config.get('imageTag', 'latest')
+    def dockerfile  = config.get('dockerfile', 'Dockerfile')
+    def buildContext = config.get('context', '.')
+
+    echo "🔨 Building Docker image '${imageName}:${imageTag}' using Dockerfile: ${dockerfile}"
+
+    sh """
+        docker build \\
+            -t ${imageName}:${imageTag} \\
+            -t ${imageName}:latest \\
+            -f ${dockerfile} \\
+            ${buildContext}
+    """
 }
